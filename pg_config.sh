01ba80c3560eef8ea4d841d7293e92b48d57c01d
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-05-30 09:38:04 -0300

    Initial commit

---
+++ pg_config.sh
@@ -0,0 +1,14 @@
+# apt-get -qqy update
+# DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+# apt-get -qqy install postgresql python-psycopg2
+# apt-get -qqy install python-sqlalchemy
+# apt-get -qqy install python-pip
+pip install --upgrade pip
+pip install sqlalchemy
+pip install werkzeug==0.8.3
+# pip install flask
+pip install Flask
+pip install Flask-Login==0.1.3
+pip install oauth2client
+pip install requests
+pip install httplib2
